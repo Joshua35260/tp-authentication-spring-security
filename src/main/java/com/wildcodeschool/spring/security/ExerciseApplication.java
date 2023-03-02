f1eaa4b9f45a3fd18d953a87a986e5e6e202d660
lgrignon <louis.grignon@protonmail.com>
2023-03-02 17:57:35 +0100

    fix indentation

--- src/main/java/com/wildcodeschool/spring/security/ExerciseApplication.java
+++ src/main/java/com/wildcodeschool/spring/security/ExerciseApplication.java
@@ -16,30 +16,30 @@ import com.wildcodeschool.spring.security.persistence.repositories.UserRepositor
 
 @SpringBootApplication
 public class ExerciseApplication {
-    private static Logger logger = LoggerFactory.getLogger(ExerciseApplication.class);
+	private static Logger logger = LoggerFactory.getLogger(ExerciseApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExerciseApplication.class, args);
 	}
 
-	@Autowired 
+	@Autowired
 	UserRepository userRepository;
-	
+
 	@EventListener
 	public void onStarted(ApplicationStartedEvent event) {
 		logger.info("application started");
 
 		// 2. Initialisation des données
 
-        logger.info("Initializing users");
-        userRepository.deleteAll();
+		logger.info("Initializing users");
+		userRepository.deleteAll();
 
-        // Ceci n'est pas à faire en production
-        List<RoleEnum> userRole = List.of(RoleEnum.USER);
-        List<RoleEnum> adminRole = List.of(RoleEnum.USER, RoleEnum.ADMINISTRATOR);
-        User user = new User("user", "user", "User", "USER", userRole);
-        User adminUser = new User("admin", "admin", "Admin", "ADMIN", adminRole);
-        userRepository.save(user);
-        userRepository.save(adminUser);
+		// Ceci n'est pas à faire en production
+		List<RoleEnum> userRole = List.of(RoleEnum.USER);
+		List<RoleEnum> adminRole = List.of(RoleEnum.USER, RoleEnum.ADMINISTRATOR);
+		User user = new User("user", "user", "User", "USER", userRole);
+		User adminUser = new User("admin", "admin", "Admin", "ADMIN", adminRole);
+		userRepository.save(user);
+		userRepository.save(adminUser);
 	}
 }
